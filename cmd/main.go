f3ed3f70308342423cb1732af7071c1e622e7d3d
valitovgaziz <a-c-v-a-l@mail.ru>
2024-08-29 06:26:13 +0500

    DB  connect

--- cmd/main.go
+++ cmd/main.go
@@ -5,24 +5,75 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ilyakaznacheev/cleanenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var Gamers = make([]Gamer, 10, 15)
 var MemoryType = "ram"
 var Size = 0
+var Done = make(chan bool)
+var PSQL PSQLConfig
+var DB *gorm.DB
 
 func main() {
+	// up server and chi routing
 	slog.Info("Init routing")
 	r := chi.NewRouter()
-	r.Post("/users", GroupGames)    // resive json Gamer{name: string, skill: num, latency: num}
+	r.Use(middleware.Logger)
+	r.Post("/users", GroupGamers)    // resive json Gamer{name: string, skill: num, latency: num}
 	r.Post("/memory", Memory)       // resive json memType{Type: string}
 	r.Post("/groupSize", GroupSize) // resive json GSize{size: number}
-	fmt.Println("Main.go")
+	go func() {
+		defer close(Done)
+		err := http.ListenAndServe(":3000", r)
+		if err != nil {
+			slog.Error("Can't statrt server ", "error", err)
+		}
+	}()
+	slog.Info("server started on 3000")
+
+	// init DB connection
+	err := cleanenv.ReadConfig("config.env", &PSQL)
+	if err != nil {
+		slog.Error("Can't read config", "error", err)
+		os.Exit(2)
+	}
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Yekaterinburg",
+		PSQL.PGHOST,
+		PSQL.PGUSER,
+		PSQL.PGPASSWORD,
+		PSQL.PGDATABASE,
+		PSQL.PGPORT,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	DB, err := db.DB()
+	if err != nil {
+		slog.Error("failed to get database", "error", err)
+		os.Exit(2)
+	}
+	err = DB.Ping()
+	if err != nil {
+		slog.Error("failed to ping database", "error", err)
+		os.Exit(2)
+	}
+	slog.Info("connected to database")
+
+	slog.Info("server is closed", "info", <-Done)
+	slog.Info("End")
 }
 
-func GroupGames(w http.ResponseWriter, r *http.Request) {
+func GroupGamers(w http.ResponseWriter, r *http.Request) {
 	var GamerP Gamer
 	err := json.NewDecoder(r.Body).Decode(&GamerP)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,7 +2,17 @@ module lg
 
 go 1.22.5
 
+require github.com/go-chi/chi v1.5.5
+
 require (
-	github.com/go-chi/chi v1.5.5
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.11 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
